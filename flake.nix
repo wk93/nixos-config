51961dfca27c75dc90906ba8f7afc03bfdaeb9dc
Wojciech Kania <wojtek@kania.sh>
2025-02-18 21:53:52 +0100

    import configuration.nix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,14 @@
 {
-  description = "A very basic flake";
+  description = "My OS";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    nixosConfigurations.t480 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
   };
 }
