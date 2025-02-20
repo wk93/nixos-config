79a46adbb61d405229b2977d14885bca74f70937
Wojciech Kania <wojtek@kania.sh>
2025-02-20 20:32:03 +0100

    add home manager

--- configuration.nix
+++ configuration.nix
@@ -2,12 +2,13 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
     ];
 
   nixpkgs.config.allowUnfree = true;
@@ -40,6 +41,10 @@
     packages = with pkgs; [
       tree
     ];
+  };
+
+  home-manager = {
+    users.wojtek = import ./home.nix;
   };
 
   # List packages installed in system profile. To search, run:
--- flake.nix
+++ flake.nix
@@ -3,12 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
 
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.t480 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-}
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [ 
+	  ./configuration.nix
+	  inputs.home-manager.nixosModules.default
+	];
+      };
+    };
+  }
+
